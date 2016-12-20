1a2e67704263add058d284dae3994231fa681cae
AdwardA <528458430@qq.com>
2016-12-20 21:28:25 +0800

    d

---
+++ app/src/main/java/com/example/z/thelastrow_client_gamedeal/BootActivity.java
@@ -0,0 +1,13 @@
+package com.example.z.thelastrow_client_gamedeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+}
