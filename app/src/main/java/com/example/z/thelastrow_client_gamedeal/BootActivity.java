7674d3c0d8f73fbd3984d8bcd52f60521cac0c8d
dasenzhong <727745374@qq.com>
2016-12-21 11:40:44 +0800

    开始广告跳转

--- app/src/main/java/com/example/z/thelastrow_client_gamedeal/BootActivity.java
+++ app/src/main/java/com/example/z/thelastrow_client_gamedeal/BootActivity.java
@@ -1,36 +1,82 @@
 package com.example.z.thelastrow_client_gamedeal;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class BootActivity extends AppCompatActivity {
-Handler handler;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BootActivity extends Activity {
+//Handler handler;
+
+    private Button pass;
+    private int passtime;
+    private Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boot);
-        handler=new Handler();
+//        handler=new Handler();
+        pass = (Button)findViewById(R.id.boot_pass);
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startMainActivity();
+            }
+        });
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent=new Intent(BootActivity.this,MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },1000);
+//    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        handler.postDelayed(new Runnable() {
+
+        passtime = 5;
+        timer = new Timer();
+
+        if (pass == null) {
+            pass = (Button)findViewById(R.id.boot_pass);
+        }
+
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent intent=new Intent(BootActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pass.setText("跳过(" + passtime + ")");
+                        passtime--;
+                        if (passtime < 0) {
+                            timer.cancel();
+                            startMainActivity();
+                        }
+                    }
+                });
             }
-        },1000);
+        },1000,1000);
+
+
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-
+    private void startMainActivity() {
+        startActivity(new Intent(BootActivity.this,MainActivity.class));
+        finish();
     }
 }
