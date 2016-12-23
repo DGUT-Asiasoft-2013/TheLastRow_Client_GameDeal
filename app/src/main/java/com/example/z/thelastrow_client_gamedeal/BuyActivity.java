f3deb437b7933090504eb9a70e6719bdb2ed2311
dasenzhong <727745374@qq.com>
2016-12-23 17:35:29 +0800

    添加了几个模块

---
+++ app/src/main/java/com/example/z/thelastrow_client_gamedeal/BuyActivity.java
@@ -0,0 +1,17 @@
+package com.example.z.thelastrow_client_gamedeal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/23.
+ */
+
+public class BuyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+}
