478399b8a8559a87cb857f458732dbefacfe70f9
AdwardA <528458430@qq.com>
2016-12-29 23:26:48 +0800

    -完成商品跳转详情页功能 -完成详情页的收藏和取消收藏 -购买功能未实现

--- app/src/main/java/com/example/z/thelastrow_client_gamedeal/GoodActivity.java
+++ app/src/main/java/com/example/z/thelastrow_client_gamedeal/GoodActivity.java
@@ -1,16 +1,31 @@
 package com.example.z.thelastrow_client_gamedeal;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.z.thelastrow_client_gamedeal.fragment.api.Server;
+import com.example.z.thelastrow_client_gamedeal.fragment.api.entity.Good;
+
+import com.example.z.thelastrow_client_gamedeal.fragment.api.entity.Like;
 import com.example.z.thelastrow_client_gamedeal.fragment.widget.ActionBarFragment;
 import com.example.z.thelastrow_client_gamedeal.fragment.widget.GoodNumberFragment;
 import com.example.z.thelastrow_client_gamedeal.fragment.widget.ToastAndDialog;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Z on 2016/12/28.
@@ -19,19 +34,29 @@ import com.example.z.thelastrow_client_gamedeal.fragment.widget.ToastAndDialog;
 public class GoodActivity extends Activity {
     ActionBarFragment actionBarFragment;
     GoodNumberFragment goodNumberFragment;
-    TextView    text_price_bottom;
+    TextView    text_price_bottom,text_title,text_price,text_name,text_area;
     Button btn_buy,btn_like,btn_comment;
 
+    Handler handler;
+    Good good;
+    Integer like_number=0;
+    Boolean like_state=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good);
+        handler=new Handler();
         actionBarFragment=(ActionBarFragment)getFragmentManager().findFragmentById(R.id.good_frag_action_bar);
         actionBarFragment.setTitle("商品详情");
 
         text_price_bottom=(TextView)findViewById(R.id.good_text_game_price_bottom);
+        text_title=(TextView)findViewById(R.id.good_text_title);
+        text_price=(TextView)findViewById(R.id.good_text_game_price);
+        text_name=(TextView)findViewById(R.id.good_text_game_name);
+        text_area=(TextView)findViewById(R.id.good_text_game_area);
+
         goodNumberFragment= (GoodNumberFragment) getFragmentManager().findFragmentById(R.id.good_frag_number);
-        goodNumberFragment.ediTextChange("30",9,text_price_bottom);
+
 
         btn_like= (Button) findViewById(R.id.good_btn_like);
         btn_like.setOnClickListener(new View.OnClickListener() {
@@ -55,17 +80,137 @@ public class GoodActivity extends Activity {
             }
         });
 
+        good= (Good) getIntent().getSerializableExtra("good");
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        try {
+            countLike();
+            Thread.sleep(500);
+            checkLike();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        text_title.setText(good.getGame_equip());
+        text_name.setText(good.getGame_name());
+        text_area.setText(good.getGame_area());
+        text_price.setText("￥"+good.getPrice());
+        text_price_bottom.setText(""+good.getPrice());
+        goodNumberFragment.ediTextChange(good.getPrice()+"",9,text_price_bottom);
     }
 
     public void btnLikeLisener() {
+        // 判断是否登录
+        if (Server.getUser()==null){
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }else {
+
+            boolean state=true;
+            String s=btn_like.getText().toString();
+            if(like_state){
+                state=false;
+                like_state=false;
+            }else {
+                state=true;
+                like_state=true;
+            }
+            MultipartBody requestBody=new MultipartBody.Builder()
+                    .addFormDataPart("likes",String.valueOf(state))
+                    .build();
+            Request request=Server.requestBuilderWithApi("good/"+good.getId()+"/likes")
+                    .post(requestBody)
+                    .build();
+            Server.getSharedClient().newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(like_state){
+                                btn_like.setText("已收藏("+countLike()+")");
+                            }else {
+                                btn_like.setText("收藏("+countLike()+")");
+                            }
+
+                        }
+                    });
+
+                }
+            });
+        }
 
     }
+
+    public boolean checkLike(){
+//        good.getAuthor().getId()
+        Request request=Server.requestBuilderWithApi("good/"+good.getId()+"/isliked")
+                .get()
+                .build();
+        Server.getSharedClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                like_state=new ObjectMapper().readValue(response.body().string(),Boolean.class);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (like_state){
+                            btn_like.setText("已收藏("+like_number+")");
+                        }
+                       // ToastAndDialog.setToastShort(getApplicationContext(),like_state.toString());
+                    }
+                });
+            }
+        });
+        return like_state;
+    }
+
+    public int countLike(){
+
+        final Request request=Server.requestBuilderWithApi("good/"+good.getId()+"/likes")
+                .get()
+                .build();
+        Server.getSharedClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                like_number= new ObjectMapper().readValue(response.body().string(),Integer.class);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(like_state){
+                            btn_like.setText("已收藏("+like_number+")");
+                        }else {
+                            btn_like.setText("收藏("+like_number+")");
+                        }
+                    }
+                });
+
+            }
+        });
+
+        return like_number;
+    }
+
+
     public void btnCommentLisener() {
     }
     public void btnBuyLisener() {
@@ -81,9 +226,26 @@ public class GoodActivity extends Activity {
             if (itemMoney>userMoney){
                 ToastAndDialog.setToastShort(getApplicationContext(),"账户余额不足");
             }else{
+                        new  AlertDialog.Builder(GoodActivity.this)
+                                .setMessage("确定购买？")
+                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        buy();
+                                    }
+                                })
+                                .setNegativeButton("取消",null)
+                                .create()
+                                .show();
+
+                    }
 
             }
-        }
+
+
+    }
+
+    public void  buy(){
 
     }
 }
--- app/src/main/java/com/example/z/thelastrow_client_gamedeal/fragment/page/NoteListFragment.java
+++ app/src/main/java/com/example/z/thelastrow_client_gamedeal/fragment/page/NoteListFragment.java
@@ -2,20 +2,23 @@ package com.example.z.thelastrow_client_gamedeal.fragment.page;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.z.thelastrow_client_gamedeal.GoodActivity;
 import com.example.z.thelastrow_client_gamedeal.R;
 import com.example.z.thelastrow_client_gamedeal.fragment.api.Server;
+import com.example.z.thelastrow_client_gamedeal.fragment.api.entity.Good;
 import com.example.z.thelastrow_client_gamedeal.fragment.api.entity.Page;
-import com.example.z.thelastrow_client_gamedeal.fragment.api.entity.good;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -37,7 +40,7 @@ public class NoteListFragment extends Fragment {
     private TextView notes_sell,notes_buy;
     private ListView notes_list;
 
-    private List<good> equipList;
+    private List<Good> equipList;
     private int page;
 
     @Nullable
@@ -52,6 +55,12 @@ public class NoteListFragment extends Fragment {
             notes_list = (ListView) view.findViewById(R.id.notes_list);
 
             notes_list.setAdapter(baseAdapter);
+            notes_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    listClick(position);
+                }
+            });
         }
         return view;
     }
@@ -83,8 +92,8 @@ public class NoteListFragment extends Fragment {
 
                 try {
 
-                    final Page<good> data = new ObjectMapper()
-                            .readValue(response.body().string(), new TypeReference<Page<good>>() {});
+                    final Page<Good> data = new ObjectMapper()
+                            .readValue(response.body().string(), new TypeReference<Page<Good>>() {});
 
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
@@ -104,6 +113,12 @@ public class NoteListFragment extends Fragment {
 
     }
 
+    public void   listClick(int position){
+        Good good=equipList.get(position);
+        Intent intent=new Intent(getActivity(), GoodActivity.class);
+        intent.putExtra("good",good);
+        startActivity(intent);
+    }
 
     BaseAdapter baseAdapter = new BaseAdapter() {
         @Override
@@ -134,7 +149,7 @@ public class NoteListFragment extends Fragment {
             TextView notes_listitem_thingsvalue = (TextView) convertView.findViewById(R.id.notes_listitem_thingsvalue);
             ImageView notes_listitem_thingspicture = (ImageView) convertView.findViewById(R.id.notes_listitem_thingspicture);
 
-            good equip = equipList.get(position);
+            Good equip = equipList.get(position);
 
             notes_listitem_gamename.setText(equip.getGame_name());
             notes_listitem_companyname.setText(equip.getGame_company());
